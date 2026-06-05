1fe3c958fafbc9eafdd036767c88920d75b4ccf9
clartmaster-sys <clartmaster@hotmail.com>
2026-06-05 18:45:13 +0200

    sgadbvdfbdafv

--- serotest.lua
+++ serotest.lua
@@ -195,15 +195,15 @@ do
 
 	local Themes = {
         ["Preset"] = {
-            ["Window Outline"] = FromRGB(255, 105, 180),
-            ["Accent"] = FromRGB(255, 105, 180),
-            ["Background 1"] = FromRGB(17, 21, 27),
+            ["Window Outline"] = FromRGB(252, 3, 123),
+            ["Accent"] = FromRGB(252, 3, 123),
+            ["Background 1"] = FromRGB(25, 25, 25),
             ["Text"] = FromRGB(255, 255, 255),
-            ["Inline"] = FromRGB(19, 25, 31),
-            ["Element"] = FromRGB(32, 38, 48),
+            ["Inline"] = FromRGB(18, 0, 9),
+            ["Element"] = FromRGB(48, 32, 38),
             ["Inactive Text"] = FromRGB(185, 185, 185),
-            ["Border"] =  FromRGB(46, 52, 61),
-            ["Background 2"] = FromRGB(255, 105, 180)
+            ["Border"] =  FromRGB(54, 4, 22),
+            ["Background 2"] = FromRGB(25, 25, 25)
         }
 	}
 
